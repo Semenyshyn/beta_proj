c9f673617e6fd06561bccce5f1f52f4bb5171db2
Semenyshyn <simen.sims@gmail.com>
2017-11-29 13:34:39 +0200

    Initial Commit work 1.0.0

---
+++ main_page/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'main_page'
+urlpatterns = [
+    url(r'^$', views.stream_data, name='stream_data'),
+    url(r'^list/$', views.message_list, name='message_list'),
+]
+
+
